0998d2b7a7d4439d59f42a0b91cf171cd22ddcfe
Murat Gültürk <m.gueltuerk@gmx.de>
2022-05-05 14:08:22 +0200

    Initial Commit

---
+++ Product Listing/Product_ListingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Product_ListingApp.swift
+//  Product Listing
+//
+//  Created by Murat Gültürk on 05.05.22.
+//
+
+import SwiftUI
+
+@main
+struct Product_ListingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
